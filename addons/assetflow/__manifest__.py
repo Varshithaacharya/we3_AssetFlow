2728e77dbc98289760010ca6d76c8cee36e09b72
Varshitha Kumari <varshitha.kumari04@gmail.com>
2026-07-12 12:05:48 +0530

    Added department_views.xml file content

--- addons/assetflow/__manifest__.py
+++ addons/assetflow/__manifest__.py
@@ -1,11 +1,51 @@
 {
     'name': 'AssetFlow',
-    'version': '1.0',
-    'depends': ['base'],
-    'author': 'Your Name',
-    'category': 'Uncategorized',
-    'description': 'AssetFlow single module',
-    'data': [],
+    'version': '1.0.0',
+    'summary': 'Enterprise Asset & Resource Management System',
+    'description': """
+AssetFlow
+=========
+Enterprise Asset & Resource Management System
+""",
+    'author': 'Team AssetFlow',
+    'category': 'Operations',
+    'license': 'LGPL-3',
+
+    'depends': [
+        'base',
+        'mail',
+        'hr',
+        'calendar',
+    ],
+
+    'data': [
+
+        # Security
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+
+        # Menus
+        'views/menus.xml',
+
+        # Organization
+        'views/department_views.xml',
+        'views/category_views.xml',
+        'views/employee_views.xml',
+
+        # Assets
+        'views/asset_views.xml',
+        'views/allocation_views.xml',
+
+        # Operations
+        'views/booking_views.xml',
+        'views/maintenance_views.xml',
+        'views/audit_views.xml',
+        'views/notification_views.xml',
+
+        # Dashboard
+        'views/dashboard_views.xml',
+    ],
+
     'installable': True,
     'application': True,
 }
